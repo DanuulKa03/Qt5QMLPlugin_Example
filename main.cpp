8857811a7a56ff5bdf7e0d5ac1d85fdd6e154a97
Mentalflow <312902918@qq.com>
2025-04-28 22:29:14 +0800

    Project: Let the example work as usual.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <QQmlApplicationEngine>
-
+#include <QtQml/qqmlextensionplugin.h>
 #include <QGuiApplication>
+
+// Add below code before your main() function
+#ifdef FORMS_BUILD_STATIC_LIB
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0)) // Add this check if you want to support both Qt 5 and Qt 6
+#define Q_IMPORT_QML_PLUGIN(PLUGIN) \
+Q_IMPORT_PLUGIN(PLUGIN)
+extern void qml_static_register_types_Forms();
+#endif
+Q_IMPORT_QML_PLUGIN(FormsPlugin)
+#endif
 
 auto main(int argc, char* argv[]) -> int
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    engine.addImportPath("qrc:/");
     engine.addImportPath("qrc:/qml");
     engine.addImportPath("qrc:/qml/UILibrary");
 
@@ -16,6 +27,11 @@ auto main(int argc, char* argv[]) -> int
                 QCoreApplication::exit(-1);
         }, Qt::QueuedConnection);
     engine.load(url);
-
+// Add below code before engine.load()
+#ifdef FORMS_BUILD_STATIC_LIB
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0)) // Add this check if you want to support both Qt 5 and Qt 6
+    qml_static_register_types_Forms();
+#endif
+#endif
     return app.exec();
 }
