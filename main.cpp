9b58015d476adee36cc1afe61304301bd22696f7
Mentalflow <312902918@qq.com>
2025-04-29 22:29:18 +0800

    Project: Add an option for shared build and more improvement.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <QGuiApplication>
 
 // Add below code before your main() function
-#ifdef FORMS_BUILD_STATIC_LIB
+#if SUBMODULE_BUILD_STATIC
 #if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0)) // Add this check if you want to support both Qt 5 and Qt 6
 #define Q_IMPORT_QML_PLUGIN(PLUGIN) \
 Q_IMPORT_PLUGIN(PLUGIN)
@@ -23,8 +23,22 @@ auto main(int argc, char* argv[]) -> int
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.addImportPath("qrc:/qml");
+#if SUBMODULE_BUILD_STATIC
+    engine.addImportPath("qrc:/qml"); // There are two way for import static module
+                                      // addImportPath or qml_static_register_types, only one should be enough
+                                      // If you see "Cyclic dependency detected between XXX and XXX" with qml_static_register_types
+                                      // It's a bug of qmlRegisterSingletonType, just ignore it.
     engine.addImportPath("qrc:/qml/UILibrary");
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0)) && 0 // Add this check if you want to support both Qt 5 and Qt 6
+    qml_static_register_types_Forms();
+    qml_static_register_types_Components();
+    qml_static_register_types_Common();
+    qml_static_register_types_Pages();
+#endif
+#else
+    engine.addImportPath(SUBMODULE_IMPORT_PATH);
+    engine.addImportPath(QString(SUBMODULE_IMPORT_PATH) + "/UILibrary");
+#endif
 
     const QUrl url("qrc:/StackView-Template/main.qml");
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app, [url](QObject* obj, const QUrl& objUrl) {
@@ -32,14 +46,5 @@ auto main(int argc, char* argv[]) -> int
                 QCoreApplication::exit(-1);
         }, Qt::QueuedConnection);
     engine.load(url);
-// Add below code before engine.load()
-#ifdef FORMS_BUILD_STATIC_LIB
-#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0)) // Add this check if you want to support both Qt 5 and Qt 6
-    qml_static_register_types_Forms();
-    qml_static_register_types_Components();
-    qml_static_register_types_Common();
-    qml_static_register_types_Pages();
-#endif
-#endif
     return app.exec();
 }
